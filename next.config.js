5dfec13d77f0df1fdf80b4d8afb6fa814457659c
JR <180821520+mscbuild@users.noreply.github.com>
2025-07-31 16:18:40 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,12 +2,50 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      {
+      {   
         protocol: "https",
         hostname: "cdn.sanity.io",
         port: "",
       },
     ],
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // применяет заголовки ко всем маршрутам
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: `
+              default-src 'self';
+              script-src 'self' 'unsafe-inline' 'unsafe-eval';
+              style-src 'self' 'unsafe-inline';
+              img-src 'self' https://cdn.sanity.io data:;
+              font-src 'self';
+              connect-src 'self';
+              frame-src 'none';
+            `.replace(/\s{2,}/g, ' ').trim(),
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+        ],
+      },
+    ];
   },
 };
 
