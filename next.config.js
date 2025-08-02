ef948623a0f3b2287f06d34ec084d5a09a3f079a
JR <180821520+mscbuild@users.noreply.github.com>
2025-08-02 16:23:20 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,9 @@
-/** @type {import('next').NextConfig} */
+ /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {   
-        protocol: "https",
-        hostname: "cdn.sanity.io",
-        port: "",
-      },
-    ],
-  },
-
   async headers() {
     return [
       {
-        source: "/(.*)", // применяет заголовки ко всем маршрутам
+        source: "/(.*)", // applies headers to all routes
         headers: [
           {
             key: "Content-Security-Policy",
@@ -49,4 +39,5 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
+
